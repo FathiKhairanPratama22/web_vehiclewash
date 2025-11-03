a9974440860017b95c43a4336cb0e3e2102c0872
nashwanazzamhilmy <nashwanhilmy@gmail.com>
2025-11-03 23:31:15 +0700

    add unique and descriptive titles to all pages

--- admin/laporan.php
+++ admin/laporan.php
@@ -38,7 +38,7 @@ if ($result_laporan) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laporan Pendapatan - ProClean</title>
+    <title>Laporan - ProClean</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
